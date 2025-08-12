5c825de925bc6e2a611a839e596383016a4acbae
srfidayee <shariefidayee21@gmail.com>
2025-08-12 20:32:41 +0530

    Generated Orders HTML in order.js

--- data/orders.js
+++ data/orders.js
@@ -8,3 +8,23 @@ export function addOrder(order) {
 function saveTOLocalStorage() {
     localStorage.setItem('orders', JSON.stringify(orders));
 }
+/*
+How order array looks like
+'[{
+    "id":"22447833-6949-40a8-b8e6-4acd84cb91c2",
+    "orderTime":"2025-08-12T11:42:26.763Z",
+    "totalCostCents":5251,
+    "products":[
+        {
+            "productId":"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            "quantity":2,
+            "estimatedDeliveryTime":"2025-08-19T11:42:26.763Z",
+            "variation":null},
+        {
+            "productId":"15b6fc6f-327a-4ec4-896f-486349e85a3d",   
+            "quantity":1,
+            "estimatedDeliveryTime":"2025-08-15T11:42:26.763Z",
+            "variation":null}
+    ]
+}]'
+*/
---
+++ scripts/orders.js
@@ -0,0 +1,96 @@
+import { orders } from "../data/orders.js";
+import { currencyFormat } from "./utils/money.js";
+import { getProduct, loadProductsFetch, products } from "../data/products.js";
+import dayJS from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+
+let ordersSummaryHTML = '';
+
+async function loadPage() {
+    await loadProductsFetch();
+    renderOrdersSummary();
+}
+
+loadPage();
+
+function renderOrdersSummary() {
+
+    orders.forEach((order) => {
+
+        ordersSummaryHTML += `
+        <div class="order-container">
+
+            <div class="order-header">
+                <div class="order-header-left-section">
+                    <div class="order-date">
+                        <div class="order-header-label">Order Placed:</div>
+                        <div>${dayJS(order.orderTime).format('MMMM D')}</div>
+                    </div>
+                    <div class="order-total">
+                        <div class="order-header-label">Total:</div>
+                        <div>$${currencyFormat(order.totalCostCents)}</div>
+                    </div>
+                </div>
+
+                <div class="order-header-right-section">
+                    <div class="order-header-label">Order ID:</div>
+                    <div>${order.id}</div>
+                </div>
+            </div>
+
+            <div class="order-details-grid">
+                ${orderItemHTML(order)}
+            </div>
+        </div>
+    `;
+
+    })
+
+    function orderItemHTML(order) {
+        let html = '';
+        console.log("Products array at runtime:", products);
+        order.products.forEach((product) => {
+            let matchingProduct = getProduct(product.productId);
+
+            if (!matchingProduct) {
+                console.warn(`Product not found for ID: ${product.productId}`);
+                console.log(order);
+                return;
+            }
+
+            html += `
+            <div class="product-image-container">
+                <img src="${matchingProduct.image}">
+            </div>
+
+            <div class="product-details">
+                <div class="product-name">
+                    ${matchingProduct.name}
+                </div>
+                <div class="product-delivery-date">
+                    Arriving on: ${dayJS(product.estimatedDeliveryTimes).format('MMMM D')}
+                </div>
+                <div class="product-quantity">
+                    Quantity: ${product.quantity}
+                </div>
+                <button class="buy-again-button button-primary">
+                    <img class="buy-again-icon" src="images/icons/buy-again.png">
+                    <span class="buy-again-message">Buy it again</span>
+                </button>
+            </div>
+
+            <div class="product-actions">
+                <a href="tracking.html">
+                    <button class="track-package-button button-secondary">
+                        Track package
+                    </button>
+                </a>
+            </div>
+        `;
+        });
+
+        return html;
+    }
+
+    document.querySelector('.orders-grid').innerHTML = ordersSummaryHTML;
+
+}
